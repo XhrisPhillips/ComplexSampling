38969ff8c2eccd902c09e716c7c33aaf104b680e
Chris Phillips <Chris.Phillips@csiro.au>
2024-09-10 17:51:59 +1000

    Add some missign files

--- createData.sh
+++ createData.sh
@@ -13,6 +13,7 @@ generateVDIF -seed=$SEED2 -year 2020 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2
 generateVDIF -seed=$SEED2 -year 2020 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -lsb TEST2-lsb.vdif
 
 # Complex (single side band)
+generateVDIF -seed=$SEED1 -year 2020 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.5 -dayno 100 -time 07:00:00 -hilbert      TEST1-complex-usb.vdif
 generateVDIF -seed=$SEED2 -year 2020 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert      TEST2-complex-usb.vdif
 generateVDIF -seed=$SEED2 -year 2020 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert -lsb TEST2-complex-lsb.vdif
 
